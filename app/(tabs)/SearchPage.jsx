d859fa9fbd78aafab6bb391ca9c8601550a19a6a
cosmicchimp <max.lambert429@gmail.com>
2025-03-29 10:41:20 -0700

    Visual changes to results page

--- app/(tabs)/SearchPage.jsx
+++ app/(tabs)/SearchPage.jsx
@@ -7,12 +7,22 @@ import {
   FlatList,
   Image,
   TouchableOpacity,
+  Modal,
+  TouchableWithoutFeedback,
+  Animated,
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons"; // You can choose from different icon sets like MaterialIcons, Ionicons, etc.
-
 export default function SearchPage() {
   const [userInput, updateUserInput] = useState("");
   const [fetchedBooks, updateBooks] = useState([]);
+  const [resultsDisplayed, updateResultsDisplayed] = useState(true);
+  useEffect(() => {
+    if (fetchedBooks.length !== 0) {
+      updateResultsDisplayed(true);
+    } else {
+      updateResultsDisplayed(false);
+    }
+  }, [fetchedBooks]);
   useEffect(() => {
     console.log(fetchedBooks);
   }, [fetchedBooks]);
@@ -27,48 +37,55 @@ export default function SearchPage() {
       .then(updateBooks)
       .catch((error) => console.error(error));
   }
-  const [currentPopUp, updatePopUp] = useState([]);
-  function handleBookPress() {
-    alert("Press");
-  }
+  const [displayModal, updateDisplay] = useState(false);
+  const [modalBook, updateModalBook] = useState(null);
   return (
-    <View style={styles.container}>
-      {/* TextInput for searching books */}
-      <View style={styles.searchbar}>
-        <Icon
-          name="search"
-          size={30}
-          color="rgba(186, 12, 47, 1)"
-          style={{
-            alignSelf: "center",
-          }}
-        />
-        <TextInput
-          style={styles.bookInput}
-          onChangeText={updateUserInput}
-          onSubmitEditing={() => fetchBook(userInput)}
-          placeholder="Type something..."
-          placeholderTextColor="lightgrey"
+    <View style={styles.backgroundContainer}>
+      <View style={styles.container}>
+        {/* TextInput for searching books */}
+        <View style={styles.searchbar}>
+          <Icon
+            name="search"
+            size={30}
+            color="rgba(186, 12, 47, 1)"
+            style={{
+              alignSelf: "center",
+            }}
+          />
+          <TextInput
+            style={styles.bookInput}
+            onChangeText={updateUserInput}
+            onSubmitEditing={() => fetchBook(userInput)}
+            placeholder="Type something..."
+            placeholderTextColor="lightgrey"
+          />
+        </View>
+        {resultsDisplayed && <Text style={styles.results}>Results</Text>}
+        <FlatList
+          data={fetchedBooks}
+          keyExtractor={(book) => book.key}
+          contentContainerStyle={{ justifyContent: "space-around" }} // Ensures no extra spacing at end
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => {
+                updateDisplay(true);
+                updateModalBook(item);
+              }}
+              style={styles.bookBox}
+            >
+              <View style={styles.bookItem}>
+                <Image
+                  source={{
+                    uri: `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`,
+                  }}
+                  style={styles.bookImage}
+                />
+                <Text style={styles.text}>{item.title}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
         />
       </View>
-      <FlatList
-        data={fetchedBooks}
-        keyExtractor={(book) => book.key}
-        contentContainerStyle={{ justifyContent: "space-around" }} // Ensures no extra spacing at end
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={handleBookPress} style={styles.bookBox}>
-            <View style={styles.bookItem}>
-              <Image
-                source={{
-                  uri: `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`,
-                }}
-                style={styles.bookImage}
-              />
-              <Text style={styles.text}>{item.title}</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-      />
     </View>
   );
 }
@@ -79,6 +96,7 @@ export default function SearchPage() {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+    backgroundColor: "rgba(186, 12, 47, .9)",
   },
   bookItem: {
     marginBottom: 20,
@@ -95,13 +113,15 @@ const styles = StyleSheet.create({
     height: 150,
     marginBottom: 10,
     marginTop: 30,
+    alignSelf: "center",
+    borderColor: "black",
+    borderWidth: 1,
+    boxShadow: "2px 2px 3px 0px black",
   },
   text: {
     color: "black",
     maxWidth: "70%",
     textAlign: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: "white",
     paddingBottom: 10,
     marginBottom: 10,
     color: "white",
@@ -114,6 +134,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  results: {
+    color: "white",
+    fontFamily: "SpaceMono-Regular",
+    fontWeight: 800,
+    fontSize: 20,
+    alignSelf: "center",
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+    width: "100%",
+    textAlign: "center",
+  },
   searchbar: {
     display: "flex",
     flexDirection: "row",
@@ -125,11 +156,25 @@ const styles = StyleSheet.create({
   },
   bookBox: {
     padding: 20,
-    width: 200,
+    width: 250,
     height: 320,
     marginBottom: 60,
     alignSelf: "center",
     borderRadius: 10,
+    alignContent: "center",
+    justifyContent: "center",
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+  },
+  closeButton: {
+    borderRadius: 5,
     backgroundColor: "rgba(186, 12, 47, 1)",
+    padding: 10,
+    minWidth: 100,
+    display: "flex",
+    alignItems: "center",
+  },
+  closeText: {
+    fontWeight: 600,
   },
 });
